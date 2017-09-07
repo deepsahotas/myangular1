f7fd7b509693d75a6e6103e50b9502c591597c5b
pardeep kumar <pardeep@traveltriangle.com>
2017-09-07 15:36:18 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var myModule = angular.module('myApp', []);
 
 myModule.factory('APIUtils', ['$http', '$q', function($http, $q){
    var utils={};
+   var deferred = $q.defer();
    $http.defaults.headers.common.Accept = 'application/json';
    utils.sendRequest=function(url){
        $http.get(url)
@@ -19,9 +20,21 @@ myModule.factory('APIUtils', ['$http', '$q', function($http, $q){
 
 
 myModule.controller('myCtrl', ['$scope','APIUtils' ,function($scope, APIUtils) {
-	APIUtils.sendRequest('../json/slides.json').then(function(data){
-		$scope.slidesData = data;
+	$scope.slidesData = [];
+  APIUtils.sendRequest('/json/slides.json').then(function(response){
+		$scope.slidesData = response.data;
 	});
-	$scope.bannerCaption = "Hongkong";
-	$scope.bannerText = "Hongkong is the beautiful city";
+  // $scope.displayBanner = function(slides){
+  //   var l = slides.length;
+  //   var start = 0;
+  //   while(l > 0){
+  //     setInterval
+  //     if(start == l){
+  //       start = 0;
+  //     }
+  //     $scope.bannerCaption = $scope.slidesData[start].banner_title;
+  //     $scope.bannerText = $scope.slidesData[start].banner_text;
+  //     start += 1; 
+  //   }
+  // }
 }]);
